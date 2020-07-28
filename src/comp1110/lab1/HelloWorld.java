1fa595263da187ea7efc6a8f325e65e324b40d27
u7172618 <ming.lei@anu.edu.au>
2020-07-29 06:49:07 +1000

    Completed Lab 1 in VDI

--- src/comp1110/lab1/HelloWorld.java
+++ src/comp1110/lab1/HelloWorld.java
@@ -1,7 +1,6 @@
 package comp1110.lab1;
 
 public class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) { System.out.println("Hello world!");
     }
 }
