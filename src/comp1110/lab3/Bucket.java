4f472a8b9cb66148cfbf52aae31e45338394fd27
u7172618 <ming.lei@anu.edu.au>
2020-08-16 20:25:34 -0700

    Done with Lab 3!

---
+++ src/comp1110/lab3/Bucket.java
@@ -0,0 +1,41 @@
+package comp1110.lab3;
+
+public class Bucket {
+    private double content = 0;
+    private double capacity;
+    public Bucket(double capacity){
+        this.capacity= capacity;
+    }
+    public double getCapacity(){
+        return capacity;
+    }
+    public double getContents(){
+        return content;
+    }
+    public double empty(){
+       double a = content;
+       content = 0;
+       return a;
+    }
+    public void add(double amount){
+        if (amount <= capacity - content ){
+            content += amount;
+        }
+        else{content = capacity;}
+    }
+
+    public static void main(String[] args) {
+        Bucket big = new Bucket(10.0);
+        Bucket small = new Bucket(1.0);
+        big.add(20);
+        small.add(20);
+        System.out.println(big.getContents());
+        System.out.println(small.getContents());
+        big.empty();
+        System.out.println(big.getContents());
+        big.add(small.empty());
+        System.out.println(small.getContents());
+        System.out.println(big.getContents());
+    }
+
+}
---
+++ src/comp1110/lab3/StackablebBucket.java
@@ -0,0 +1,46 @@
+package comp1110.lab3;
+
+class StackableBucket extends Bucket {
+    private final String name;
+    private StackableBucket innerBucket = null;
+    public StackableBucket (double capacity, String name) {
+        super(capacity);
+        this.name = name;
+    }
+    public String getInnerBucket(){
+        if (innerBucket != null) {return this.innerBucket.name;}
+        else {return null;}
+    }
+    public void setInnerBuckets(StackableBucket smallerBucket){
+        if (smallerBucket.getCapacity() >= this.getCapacity()){
+            System.out.println("Too large to stack!");
+        }
+        if (this.innerBucket == null && smallerBucket.getCapacity() <= this.getCapacity()){
+            this.innerBucket = smallerBucket;
+        }
+        else if (this.innerBucket != null){
+            this.innerBucket.setInnerBuckets(smallerBucket);
+        }
+    }
+    public void unstackBuckets(){
+        if (this.innerBucket != null) {
+            this.innerBucket.unstackBuckets();
+            this.innerBucket = null;
+        }
+    }
+
+    public static void main(String[] args) {
+        StackableBucket big = new StackableBucket(10.0, "big");
+        StackableBucket medium = new StackableBucket(5.0, "medium");
+        StackableBucket small = new StackableBucket(1.0, "small");
+        medium.setInnerBuckets(big);
+        big.setInnerBuckets(medium);
+        System.out.println(big.getInnerBucket());
+        big.setInnerBuckets(small);
+        System.out.println(big.getInnerBucket());
+        System.out.println(medium.getInnerBucket());
+        big.unstackBuckets();
+        System.out.println(big.getInnerBucket());
+        System.out.println(medium.getInnerBucket());
+    }
+}
