290996c424f6f5c0090f0208c142e05f111b5cd3
Josh Milthorpe <josh.milthorpe@anu.edu.au>
2020-10-05 20:32:39 +1100

    new exercises for Lab 8: FamilyTree and Stack

--- src/comp1110/lab8/ArrayStack.java
+++ src/comp1110/lab8/ArrayStack.java
@@ -2,46 +2,84 @@ package comp1110.lab8;
 
 import java.util.EmptyStackException;
 
-public class ArrayStack<T> implements Stack<T> {
-    private static final int INITIAL_SIZE = 2;
-    private static final float GROWTH_FACTOR = 1.5f;
-
-    int topIndex = 0;
-    private T[] values = (T[]) new Object[INITIAL_SIZE];
-
-    @Override
+/**
+ * This class represents a stack, in which elements are added and removed in a
+ * last in, first out (LIFO) order. Duplicate elements are permitted.
+ * When a stack is first created, it contains no elements.
+ * The stack can grow to fit new elements as required.
+ * Attempting to pop() or peek() an empty stack throws an EmptyStackException,
+ * and does not result in any modification to the stack.
+ * The Stack is implemented using an array data structure (a regular Java array),
+ * and does not use any of the Java Collection classes other than
+ * java.util.EmptyStackException.
+ */
+public class ArrayStack<T> {
+    /**
+     * @return true if the stack is empty
+     */
     public boolean isEmpty() {
-        return topIndex == 0;
+        // FIXME complete this method
+        return false;
     }
 
-    @Override
+    /**
+     * Add the given value to this stack,
+     * placing it at the top of the stack.
+     *
+     * @param value the value to add to this stack
+     */
     public void push(T value) {
-        if (topIndex == values.length - 1) {
-            // potential error - out of space
-            T[] newValues = (T[]) new Object[(int) Math.ceil(values.length * GROWTH_FACTOR)];
-            System.arraycopy(values, 0, newValues, 0, values.length);
-            values = newValues;
-        }
-        values[topIndex++] = value; // potential error - failure to maintain index
+        // FIXME complete this method
     }
 
-    @Override
+    /**
+     * Remove the value that is at the top of this stack, and return it.
+     *
+     * @return the value that was popped from the stack
+     * @throws EmptyStackException if the stack is currently empty
+     */
     public T pop() {
-        if (topIndex == 0) {
-            // potential error - exception if empty
-            throw new EmptyStackException();
-        }
-        T result = values[--topIndex]; // potential error - failure to maintain index
-        values[topIndex] = null; // potential error - forgetting to null out value
-        return result;
+        // FIXME complete this method
+        return null;
     }
 
-    @Override
+    /**
+     * Get the value that is currently at the top of this stack,
+     * but do not remove it from the stack.
+     *
+     * @return the value at the top of the stack
+     * @throws EmptyStackException if the stack is currently empty
+     */
     public T peek() {
-        if (topIndex == 0) {
-            // potential error - exception if empty
-            throw new EmptyStackException();
-        }
-        return values[topIndex - 1]; // potential error - wrong top index
+        // FIXME complete this method
+        return null;
+    }
+
+    /**
+     * Check whether a given value is contained in this stack.
+     * Specifically, returns true if value is not null and
+     * an element e is contained in the stack such that e.equals(value).
+     *
+     * @param value the value to search for
+     * @return true if the value is contained in this stack
+     */
+    public boolean contains(T value) {
+        // FIXME complete this method
+        return false;
+    }
+
+    /**
+     * Create a String representation of this stack.
+     * Elements on the stack are listed in order from top to bottom,
+     * separated by commas (without spaces).
+     * If the stack is empty, an empty string is returned.
+     * For example, a stack containing the elements (from top to bottom)
+     * "a", "b", and "c" would be represented as "a,b,c".
+     *
+     * @return a String representation of this stack
+     */
+    public String toString() {
+        // FIXME complete this method
+        return null;
     }
 }
